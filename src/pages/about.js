cc89c8bfd5e53a951e654986bcf73e3113ec29a7
Vale <vale@MacBook-Pro.local>
2020-09-27 13:38:13 +0200

    added link about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "./seo"
+
+const AboutWrapper = styled.div`
+  padding: 0 50px;
+`
 
 const About = () => (
   <Layout>
     <SEO title="About Me" />
-    <h1>About me</h1>
-    <h4>
-      <i>Hey there!</i>
-    </h4>
-    <p>
-      My name is Valentina and I am a Front-end Software Engineer specializing
-      in Design Systems &{" "}
-      <a
-        href="https://en.wikipedia.org/wiki/User-centered_design"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        UCD
-      </a>
-      .
-    </p>
-    <Link to="/">
-      <span role="img" aria-label="back-emoji">
-        🔙 Home
-      </span>
-    </Link>
+    <AboutWrapper>
+      <h1>About me</h1>
+      <h4>
+        <i>Hey there!</i>
+        <span role="img" aria-label="wave-hand-emoji">
+          {" "}
+          👋
+        </span>
+      </h4>
+      <p>
+        My name is Valentina and I am a Front-end Software Engineer specializing
+        in Design Systems &{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/User-centered_design"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          UCD
+        </a>
+        .
+      </p>
+      <p>
+        <em>Disclaimer:</em> I am not a huge fan of "About Me" pages in general,
+        but in case you're curious to get to know me a bit better you can take a
+        look at{" "}
+        <a
+          href="https://www.16personalities.com/profiles/c467796fe658b"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          this link
+        </a>
+        .
+      </p>
+      <Link to="/">
+        <span role="img" aria-label="back-emoji">
+          🔙
+        </span>
+      </Link>
+    </AboutWrapper>
   </Layout>
 )
 
