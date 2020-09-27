de81d9aa817a5e3771e943fbd03a754979bae336
Vale <vale@MacBook-Pro.local>
2020-09-27 13:53:43 +0200

    added styled home image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
+import styled from "styled-components"
 import Image from "../components/Image"
 import LandingBio from "../components/LandingBio"
 import Layout from "../components/Layout"
 import Footer from "../components/Footer"
 import SEO from "./seo"
 
+const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 100px;
+`
 const IndexPage = () => (
   <>
     <Layout>
@@ -13,7 +20,9 @@ const IndexPage = () => (
         keywords={[`gatsby`, `portfolio`, `react`, `developer`]}
       />
       <LandingBio />
-      <Image fadeIn />
+      <ImgWrapper>
+        <Image fadeIn />
+      </ImgWrapper>
     </Layout>
     <Footer />
   </>
