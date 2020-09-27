d479a4e67b4c416ea097165bb86d765409e801c6
Vale <vale@MacBook-Pro.local>
2020-09-27 10:07:53 +0200

    added support code & styles.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,9 @@ import React from "react"
 import { ThemeProvider } from "./src/context/theme-context"
 import GlobalStyles from "./src/styles/GlobalStyles"
 
+import "./css-utils/prism-atom-dark.css"
+import "./css-utils/prism-line-numbers.css"
+
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>
     <>
--- src/pages/about.js
+++ src/pages/about.js
@@ -8,9 +8,26 @@ const About = () => (
   <Layout>
     <SEO title="About Me" />
     <h1>About me</h1>
-    <h4><i>Hey there!</i></h4>
-    <p>My name is Valentina and I am a Front-end Software Engineer with a passion for working with Design Systems & UCD (User Centered Design).</p>
-    <Link to="/"><span role="img" aria-label="back-emoji">🔙 Home</span></Link>
+    <h4>
+      <i>Hey there!</i>
+    </h4>
+    <p>
+      My name is Valentina and I am a Front-end Software Engineer specializing
+      in Design Systems &{" "}
+      <a
+        href="https://en.wikipedia.org/wiki/User-centered_design"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        UCD
+      </a>
+      .
+    </p>
+    <Link to="/">
+      <span role="img" aria-label="back-emoji">
+        🔙 Home
+      </span>
+    </Link>
   </Layout>
 )
 
