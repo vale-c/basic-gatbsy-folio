19031d08ea2c57d2e8bfc01d28eb790aed18aa84
Valentina <valent95@gmail.com>
2020-12-05 18:03:57 +0100

    fixed build issue with GlobalStyles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,18 @@
-import React from "react"
-import { ThemeProvider } from "./src/context/theme-context"
+import React from 'react'
+import { ThemeProvider } from './src/context/theme-context'
 
-import "./css-utils/prism-atom-dark.css"
-import "./css-utils/prism-line-numbers.css"
+import './css-utils/prism-atom-dark.css'
+import './css-utils/prism-line-numbers.css'
 
-import "./src/styles/tailwind.css"
+import './src/styles/tailwind.css'
+import GlobalStyles from './src/styles/GlobalStyles'
 
 // trigger an immediate page refresh when an update is found
 export const onServiceWorkerUpdateReady = () => window.location.reload()
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>
+    <GlobalStyles />
     <>{element}</>
   </ThemeProvider>
 )
